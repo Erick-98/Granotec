8ce8dbf32918eb07b16d46357c4f299d2e1944e8
Erick <erickva2512@gmail.com>
2025-10-01 16:57:36 -0500

    feat: agregar estructura inicial con Angular, Spring Boot y MySQL

---
+++ backend/src/main/java/com/granotec/inventory_api/GranotecInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package com.granotec.inventory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranotecInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranotecInventorySystemApplication.class, args);
+	}
+
+}
