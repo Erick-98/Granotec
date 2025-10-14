1451ed658a0d92a475e9f7f943fa31b5e24f3d70
Erick <erickva2512@gmail.com>
2025-10-14 13:09:09 -0500

    feat: creación de clase permisos

---
+++ backend/src/main/java/com/granotec/inventory_api/permission/Permission.java
@@ -0,0 +1,23 @@
+package com.granotec.inventory_api.permission;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "permissions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
