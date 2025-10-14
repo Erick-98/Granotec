96950087a0694b8df5baf64d2eae5cec20d050a0
Erick <erickva2512@gmail.com>
2025-10-14 13:09:44 -0500

    feat: creación de clase roles

---
+++ backend/src/main/java/com/granotec/inventory_api/role/Role.java
@@ -0,0 +1,34 @@
+package com.granotec.inventory_api.role;
+
+import com.granotec.inventory_api.permission.Permission;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "roles_permissions",
+            joinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions;
+
+}
+
