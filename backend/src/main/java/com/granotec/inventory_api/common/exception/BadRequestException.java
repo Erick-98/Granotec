4dd768c62afb69f1e2cf542c75abb86664ceb97f
Erick <erickva2512@gmail.com>
2025-10-15 11:11:22 -0500

    feat: agregar estandarizacion de respuestas de los endpoint

---
+++ backend/src/main/java/com/granotec/inventory_api/common/exception/BadRequestException.java
@@ -0,0 +1,5 @@
+package com.granotec.inventory_api.common.exception;
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String message) {super(message);}
+}
