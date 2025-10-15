d047bcdbf9ccd108f04183a084c79b6de64ebcab
Erick <erickva2512@gmail.com>
2025-10-15 11:18:42 -0500

    feat(auth): extender lógica en userDetailsService

--- backend/src/main/java/com/granotec/inventory_api/config/AppConfig.java
+++ backend/src/main/java/com/granotec/inventory_api/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.granotec.inventory_api.config;
 
-import com.granotec.inventory_api.user.User;
 import com.granotec.inventory_api.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -9,10 +8,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,17 +23,37 @@ public class AppConfig {
 
     private final UserRepository repository;
 
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        return username -> {
+//            final User user = repository.findByEmail(username)
+//                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+//            return org.springframework.security.core.userdetails.User
+//                    .builder()
+//                    .username(user.getEmail())
+//                    .password(user.getPassword())
+//                    .build();
+//        };
+//    }
+
     @Bean
     public UserDetailsService userDetailsService(){
-        return username -> {
-            final User user = repository.findByEmail(username)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-            return org.springframework.security.core.userdetails.User
-                    .builder()
-                    .username(user.getEmail())
-                    .password(user.getPassword())
-                    .build();
-        };
+        return username -> repository.findByEmail(username)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .builder()
+                        .username(user.getEmail())
+                        .password(user.getPassword())
+                        .authorities(user.getRoles().stream()
+                                .flatMap(role -> {
+                                    List<SimpleGrantedAuthority> auths = new ArrayList<>();
+                                    auths.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+                                    role.getPermissions().forEach(p ->
+                                            auths.add(new SimpleGrantedAuthority(p.getName())));
+                                    return  auths.stream();
+                                })
+                                .toList())
+                        .build()
+                ).orElseThrow(()-> new UsernameNotFoundException("User not found"));
     }
 
     @Bean
