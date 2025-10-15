5afa7f1724bd6952fd3ef1596228b95905742dde
Erick <erickva2512@gmail.com>
2025-10-15 11:15:56 -0500

    feat: agregar estandarizacion de sof delete

---
+++ backend/src/main/java/com/granotec/inventory_api/common/model/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.granotec.inventory_api.common.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Column(name = "is_deleted",nullable = false)
+    private Boolean isDeleted = false;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.isDeleted = this.isDeleted == null ? false : this.isDeleted;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void softDelete() {
+        this.isDeleted = true;
+        this.deletedAt = LocalDateTime.now();
+    }
+}
