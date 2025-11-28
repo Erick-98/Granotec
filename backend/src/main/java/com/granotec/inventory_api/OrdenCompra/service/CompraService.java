d94d8cf9cdf059886bdb882e9264e82f230b8f96
Erick <erickva2512@gmail.com>
2025-11-28 00:03:14 -0500

    feat : cambio en las compras service

---
+++ backend/src/main/java/com/granotec/inventory_api/OrdenCompra/service/CompraService.java
@@ -0,0 +1,13 @@
+package com.granotec.inventory_api.OrdenCompra.service;
+
+import com.granotec.inventory_api.OrdenCompra.dto.CompraRequest;
+import com.granotec.inventory_api.OrdenCompra.dto.CompraResponse;
+
+import java.util.List;
+
+public interface CompraService {
+    CompraResponse registrarCompra(CompraRequest request);
+    CompraResponse obtenerCompra(Integer id);
+    List<CompraResponse> listarCompras();
+}
+
